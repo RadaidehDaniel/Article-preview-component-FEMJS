827ebb31a1badcd9bedea67533306f6bd6e8f825
RadaidehDaniel <mohammadxradaideh@gmail.com>
2024-05-29 21:45:52 +0300

    last update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const btn = document.querySelector(".btn");
+const shareBox = document.querySelector(".share-container");
 
+btn.addEventListener("click", () => {
+  shareBox.classList.toggle("hide");
+});
+
+btn.addEventListener("focusout", () => {
+  shareBox.classList.add("hide");
+});
